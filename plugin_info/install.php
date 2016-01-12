edde842165963c7acd3a76d01469595f5ae55207
loic <lgevrey@oxya.com>
2016-01-12 19:07:18 +0100

    Typo

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -19,8 +19,8 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 function denonavr_update() {
-	foreach (camera::byType('denonavr') as $camera) {
-		$camera->save();
+	foreach (denonavr::byType('denonavr') as $denonavr) {
+		$denonavr->save();
 	}
 }
 
