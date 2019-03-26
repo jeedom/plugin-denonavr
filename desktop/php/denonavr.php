56ed74f1c84a281c296a202f610307e57c9df700
Loïc <loic@jeedom.com>
2019-03-26 16:26:01 +0100

    uniformize

--- desktop/php/denonavr.php
+++ desktop/php/denonavr.php
@@ -17,15 +17,15 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				<span >{{Ajouter}}</span>
 			</div>
 		</div>
-		
 		<legend><i class="fas fa-table"></i> {{Mes Denons}}</legend>
 		<input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
 		<div class="eqLogicThumbnailContainer">
 			<?php
 			foreach ($eqLogics as $eqLogic) {
-				echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+				$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+				echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
 				echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
-				echo "<br>";
+				echo '<br>';
 				echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
 				echo '</div>';
 			}
